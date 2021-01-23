ec7b46f70b99454633bd22bdb9aa12972b69f45e
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2021-01-23 23:42:39 +0000

    log currently active users, (u)->(i)

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ const request = require("request");
 require("dotenv").config();
 
 
+const usersLog = {}
+
 
 // const {startDatabase} = require("./database/neo");
 // const {getUsers} = require("./database/users");
@@ -12,16 +14,20 @@ require("dotenv").config();
 const neode = require("neode")
   .fromEnv()
   .with({
-    User: require("./models/User")
+    User: require("./models/User"),
+    Image: require("./models/Image")
   });
 
 const app = express();
 
-const users = [
-  { username: "TwinklePaws52", imgUrl: "https://cdn2.thecatapi.com/images/yTTGK6YL1.jpg" },
-  { username: "Peanut", imgUrl: "https://cdn2.thecatapi.com/images/u1.jpg" },
-  { username: "Sparkle", imgUrl: "https://cdn2.thecatapi.com/images/dv3.jpg" }
-];
+// app.use(session({
+//   secret: process.env.SESSION_SECRET,
+//   // genid: function(req) {
+//   //   return genuuid()
+//   // },
+//   saveUninitialized: false,
+//   resave: false
+// }));
 
 // app.user(helmet())
 
@@ -45,12 +51,22 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-// get all users
+// log in user
+// TODO send cat info
 app.get("/user/:username", (req, res) => {
-  neode.first("User", "username", req.params.username).then((user) => {
+  const username = req.params.username;
+  neode.first("User", "username", username).then((user) => {
     if (!user) {
       res.send({ success: false });
     } else {
+
+      if (usersLog[username]) {
+        usersLog[username].loggedIn = true;
+      } else {
+        usersLog[username] = {loggedIn: true}
+      }
+
+
       res.send({
         success: true,
         user: {
@@ -58,42 +74,87 @@ app.get("/user/:username", (req, res) => {
           imgUrl: user.get("img_url")
         }
       });
+      // sess = req.session;
+      // sess.username = user.get("username");
+      // sess.logged_in = true;
     }
   });
 });
 
 // create new user
 app.put("/user/:username", (req, res) => {
-  neode
-    .create("User", {
-      username: req.params.username,
-      imgUrl: req.body.img_url
-    })
-    .then((u) => {
-      res.send({
+  //TODO probably should check for already exists
+
+  const username = req.params.username;
+  const imgUrl = req.body.imgUrl
+
+  neode.writeCypher(
+    `match (i:Image {url:"${imgUrl}"})
+    create (u:User {username: "${username}"})-[:HasImage]->(i)
+    return u`
+  ).then(r => {
+    let u = neode.hydrateFirst(r, "u");
+    if (!u) {
+      res.json({success: false});
+    } else {
+      res.json({
         success: true,
         user: {
           username: u.get("username"),
-          imgUrl: u.get("img_url")
+          imgUrl: u.get("imgUrl")
         }
       });
-    });
+    }
+  }).catch(err => {
+    console.log(err);
+    res.json({success: false});
+  });
+
+
+
+  // neode
+  //   .create("User", {
+  //     username: req.params.username,
+  //     imgUrl: req.body.img_url
+  //   })
+  //   .then((u) => {
+      
+  //     neode.writeCypher(`match (u:User {username: "${u.get("username")}"}),(i:Image {url: "${u.get("imgUrl")}"}) 
+  //         create (u)-[:HasImage]->(i)`)
+  //       .then(r => {
+  //         res.json({
+  //           success: true,
+  //           user: {
+  //             username: u.get("username"),
+  //             imgUrl: u.get("imgUrl")
+  //           }
+  //         });
+  //       }).catch(err => {
+  //         console.log(err);
+  //         res.json({success: false});
+  //       });
+  //   }).catch(err => {
+  //     console.log(err);
+  //     res.json({success: false});
+  //   });
 });
 
 
 // get next user that the current user hasn't swiped on yet
+//TODO return cat info
+// TODO improve selection algorithm
 app.get("/nextuser", (req, res) => {
   neode
     .cypher(
       `match (b:User) where not (b)-[:Swiped]-(:User {username: "${req.query.currentuser}"}) and b.username <> "${req.query.currentuser}" return b`
     )
     .then((r) => {
-      let u = neode.hydrateFirst(r, "b");
+      let u = neode.hydrateFirst(r, "b"); //todo improve this
       console.log(u);
       if (!u) {
         res.send({ success: false });
       } else {
-        res.send({
+        res.json({
           success: true,
           user: {
             username: u.get("username"),
@@ -104,8 +165,10 @@ app.get("/nextuser", (req, res) => {
     });
 });
 
+//todo return list of next users
+
 // add a relationship to the graph
-app.put("/swipe", (req, res) => {
+app.post("/swipe", (req, res) => {
   let currentuser = req.body.currentuser;
   let onuser = req.body.onuser;
   let direction = req.body.direction;
@@ -120,21 +183,47 @@ app.put("/swipe", (req, res) => {
     )
     .then((r) => {
       res.send({ success: true });
+    }).catch(err => {
+      console.log(err);
+      res.json({success: false});
     });
 });
 
-
+// TODO save cat info in db
 app.get("/images", (req,res) => {
   let count = req.query.count;
   let apiKey = process.env.CAT_API_KEY;
 
-  request("https://api.thecatapi.com/v1/images/search?limit=10", {json: true, headers: {"x-api-key": apiKey}}, (err, r, body) => {
+  request(`https://api.thecatapi.com/v1/images/search?limit=${count}`, {json: true, headers: {"x-api-key": apiKey}}, (err, r, body) => {
     if (err) {res.send({success: false});}
+
+    // body = [{
+    //   "breeds": [],
+    //   "categories": [
+    //     {
+    //       "id": 1,
+    //       "name": "hats"
+    //     }
+    //   ],
+    //   "id": "MTY1ODc5MA",
+    //   "url": "https://cdn2.thecatapi.com/images/MTY1ODc5MA.png",
+    //   "width": 638,
+    //   "height": 431
+    // }]
 
     let images = []
 
-    for (var i = 0; i < 10; i++) {
+    for (let i = 0; i < count; i++) {
       images.push(body[i].url);
+
+      console.log(body[i]);
+
+      // save image data
+      neode.create("Image", {
+        url: body[i].url,
+        breeds: body[i].breeds ? JSON.stringify(body[i].breeds) : "[]",
+        categories: body[i].categories ? JSON.stringify(body[i].categories) : "[]"
+      })
     }
     res.send({
       success: true,
@@ -144,8 +233,18 @@ app.get("/images", (req,res) => {
 })
 
 
+app.get("/logout/:username", (req,res) => {
+  const username = req.params.username;
+  if (usersLog[username]) {
+    usersLog[username].loggedIn = false;
+    res.json({success: true});
+  } else {
+    res.json({success: false})
+  }
+})
+
+
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
 
-// })
