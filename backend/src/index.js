86ba6541de39126abfb1fef8653258105b002733
Martin Walls <44908692+martin-walls@users.noreply.github.com>
2021-01-23 17:50:55 +0000

    working put and get user

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,9 +16,9 @@ const neode = require("neode")
 const app = express();
 
 const users = [
-  { username: "TwinklePaws52", img_url: "https://cdn2.thecatapi.com/images/yTTGK6YL1.jpg" },
-  { username: "Peanut", img_url: "https://cdn2.thecatapi.com/images/u1.jpg" },
-  { username: "Sparkle", img_url: "https://cdn2.thecatapi.com/images/dv3.jpg" }
+  { username: "TwinklePaws52", imgUrl: "https://cdn2.thecatapi.com/images/yTTGK6YL1.jpg" },
+  { username: "Peanut", imgUrl: "https://cdn2.thecatapi.com/images/u1.jpg" },
+  { username: "Sparkle", imgUrl: "https://cdn2.thecatapi.com/images/dv3.jpg" }
 ];
 
 // app.user(helmet())
@@ -26,28 +26,83 @@ const users = [
 app.use(bodyParser.json());
 // app.use(cors());
 
+// app.get("/", (req, res) => {
+//   neode.all("User").then((u) => {
+//     console.log(u.get(0).get("username"));
+
+//     let usernames = [];
+//     for (let i = 0; i < u.length; i++) {
+//       usernames.push(u.get(i).get("username"));
+//     }
+
+//     res.send(usernames);
+//   });
+// });
+
 app.get("/", (req, res) => {
-  neode.all("User").then((u) => {
-    console.log(u.get(0).get("username"));
+  res.send("Hello world");
+});
 
-    let usernames = [];
-    for (let i = 0; i < u.length; i++) {
-      usernames.push(u.get(i).get("username"));
+// get all users
+app.get("/user/:username", (req, res) => {
+  neode.first("User", "username", req.params.username).then((user) => {
+    if (!user) {
+      res.send({ success: false });
+    } else {
+      res.send({
+        success: true,
+        user: {
+          username: user.get("username"),
+          imgUrl: user.get("img_url")
+        }
+      });
     }
-
-    res.send(usernames);
   });
 });
 
 // create new user
 app.put("/user/:username", (req, res) => {
-  neode.create("User", {
-    username: req.params.username,
-    img_url: req.body.img_url
-  }).then(u => {
-    res.send({success: true, username: u.get("username"), img_url: u.get("img_url")});
-  });
+  neode
+    .create("User", {
+      username: req.params.username,
+      imgUrl: req.body.img_url
+    })
+    .then((u) => {
+      res.send({
+        success: true,
+        user: {
+          username: u.get("username"),
+          imgUrl: u.get("img_url")
+        }
+      });
+    });
 });
+
+// doesn't quite work yet
+app.get("/nextuser", (req, res) => {
+  neode
+    .cypher(
+      `match (b:User) where not (b)-[:Swiped]-(:User {username: "${req.query.currentuser}"}) and b.username <> "${req.query.currentuser}" return b`
+    )
+    .then((r) => {
+      let u = r.hydrate();
+      console.log(u);
+      if (!u) {
+        res.send({ success: false });
+      } else {
+        res.send({
+          success: true,
+          user: {
+            username: u.get("username"),
+            imgUrl: u.get("img_url")
+          }
+        });
+      }
+    });
+});
+
+// todo
+// app.put("/swipe")
 
 // startDatabase().then(async () => {
 
